768e199b584ee8582728337b933c6c7ebfdef01b
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-10-27 08:23:15 +0700

    small fix

--- src/Client.py
+++ src/Client.py
@@ -80,7 +80,6 @@ class Client:
                 
                 src.Log.print_with_color("[>>>] Client sent parameters to server", "red")
                 self.send_to_server(data)
-                time.sleep(20)
             return True
         elif action == "STOP":
             print("Training completed. Client stopping.")
