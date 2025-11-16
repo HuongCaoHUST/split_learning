b8a2b99a6b9820bd584f93d2f0966943bac4f94e
HuongCaoHUST <caoquyhuong1662003@gmail.com>
2025-11-16 14:26:06 +0700

    fix val path

--- src/Validation.py
+++ src/Validation.py
@@ -21,7 +21,7 @@ class ModelValidator:
         print("Best model layer 1 full: ", self.best_model_layer_1)
         merge_model = self.merge_yolo_models()
         model = YOLO(merge_model)
-        metrics = model.val(data="./datasets/coco8.yaml")
+        metrics = model.val(data=self.dataset_path[0])
         print("metrics: ", metrics)
         return True
     
